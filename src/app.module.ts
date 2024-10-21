55e6fa092f70bc52750de8e6ae958673c585df05
Jiyun Jeong <wjdwldbsskfk@gmail.com>
2024-10-21 20:49:21 +0900

    세션, 쿠키, 환경변수 셋팅 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true, // 모든 모듈에서 접근 가능하도록 설정
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as hbs from 'hbs';
 import { join } from 'path';
+import * as hbs from 'hbs';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  //.env 파일 가져오기
+  const configService = app.get(ConfigService);
 
   // 정적 파일 경로 설정
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  
+
   // 뷰 파일 경로 설정
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
 
   // Partial 폴더 경로 등록
   hbs.registerPartials(join(__dirname, '..', 'views/partials'));
-  
+
   // hbs (Handlebars) 엔진 설정
   app.setViewEngine('hbs');
-  
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 세션 및 쿠키 설정
+  app.use(cookieParser());
+  app.use(session({
+    secret: configService.get<string>('SECRET_KEY') || 'default_secret', // 비밀 키 설정
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: configService.get<string>('NODE_ENV') === 'production' }, // 프로덕션 환경에서는 secure 쿠키 설정
+  }));
+
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
