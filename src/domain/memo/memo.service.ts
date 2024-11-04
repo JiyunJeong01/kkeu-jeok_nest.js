e546cfe72f078df2f70cbb7f2a7c6da5d9a7275a
Jiyun Jeong <wjdwldbsskfk@gmail.com>
2024-11-04 15:53:45 +0900

    memo create 부분 추가

--- src/domain/memo/memo.service.ts
+++ src/domain/memo/memo.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { collection, doc, getDocs, getDoc, addDoc, updateDoc, deleteDoc, query, orderBy, where } from 'firebase/firestore';
-import { db } from '../../firebase/fbase';
+import { collection, doc, getDocs, addDoc, updateDoc, deleteDoc, query, orderBy, where, limit } from 'firebase/firestore';
+import { db, storage } from '../../firebase/fbase';
+import { v4 as uuidv4 } from 'uuid';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 @Injectable()
 export class MemoService {
@@ -54,6 +56,71 @@ export class MemoService {
             return files;
         } catch (error) {
             console.error("파일 가져오기 중 오류:", error);
+            throw error;
+        }
+    }
+
+    // 메모 생성
+    async createMemo(userId: string, content: string, files?: Array<Express.Multer.File>): Promise<string> {
+        try {
+            const timestamp = new Date(); // 현재 타임스탬프
+    
+            // 새로운 메모 객체 생성
+            const memoData = {
+                userId,
+                content,
+                createdAt: timestamp,
+                updatedAt: timestamp
+            };
+    
+            // 'memos' 컬렉션에 새로운 메모 문서 추가
+            const docRef = await addDoc(collection(db, 'memos'), memoData);
+    
+            if (files && files.length > 0) {
+                await this.uploadFile(userId, docRef.id, files);
+            }
+
+            return docRef.id; // 새로 추가된 메모의 ID 반환
+        } catch (error) {
+            console.error("메모 추가 중 오류:", error);
+            throw error;
+        }
+    }
+
+    // 메모 생성시 파일 업로드 및 Firestore에 이미지 정보 저장
+    private async uploadFile(userId: string, memoId: string, files: Array<Express.Multer.File>): Promise<void> {
+        try {
+            const filesQuery = query(
+                collection(db, 'files'),
+                where('memoId', '==', memoId),
+                orderBy('index', 'desc'),
+                limit(1)
+            );
+
+            const querySnapshot = await getDocs(filesQuery);
+            let lastIndex = querySnapshot.empty ? 0 : querySnapshot.docs[0].data().index;
+
+            await Promise.all(files.map(async (file, index) => {
+                const fileName = Buffer.from(file.originalname, 'ascii').toString('utf8');
+                const uuid = uuidv4();
+                const fileRef = ref(storage, `${userId}/${uuid}_${fileName}`);
+                
+                await uploadBytes(fileRef, file.buffer);
+                const downloadURL = await getDownloadURL(fileRef);
+
+                const fileMetadata = {
+                    memoId,
+                    fileName,
+                    type: file.mimetype,
+                    uuid,
+                    index: lastIndex + index + 1,
+                    downloadURL,
+                };
+
+                await addDoc(collection(db, 'files'), fileMetadata);
+            }));
+        } catch (error) {
+            console.error("파일 업로드 및 저장 중 오류:", error);
             throw error;
         }
     }
