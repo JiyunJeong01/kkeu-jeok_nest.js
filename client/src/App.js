45b5c958e2ebe83484307a6660c5270f94128389
JiyunJeong01 <wjdwldbsskfk@naver.com>
2025-02-28 17:34:17 +0900

    로그인 컴포넌트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from './routes/Home';
 import Memo from './routes/Memo';
+import Login from './routes/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/memo" element={<Memo/>}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
   );
 }
