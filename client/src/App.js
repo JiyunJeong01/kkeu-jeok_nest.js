9cce00090b7497341bfe1931e3139b5bd47d4a5f
JiyunJeong01 <wjdwldbsskfk@naver.com>
2025-03-01 16:03:02 +0900

    로그인 시 / URL이 memo 컴포넌트를 랜더링 하도록 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom"; // 라우팅 도와주는 모듈
+import { useLogin } from './contexts/LoginContext';
 import Home from './routes/Home';
 import Memo from './routes/Memo';
 import Login from './routes/Login';
 
 function App() {
+  const { user } = useLogin();
+  
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={user ? <Memo /> : <Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
   );
