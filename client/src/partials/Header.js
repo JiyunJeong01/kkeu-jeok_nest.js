2ec0fb508c112be1731d645c72d5969c459122e5
JiyunJeong01 <wjdwldbsskfk@naver.com>
2025-03-03 17:25:39 +0900

    Header sticky 오류 해결
    부모 컴포넌트로 body를 가지도록 수정하여 해

--- client/src/partials/Header.js
+++ client/src/partials/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
             })
-            
+
             const result = await response.json();
 
             if (result.success) {
@@ -28,43 +28,41 @@ const Header = () => {
     };
 
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-blue">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">
-                        <img src="header-logo.png" alt="로고 이미지" height="40" />
-                    </Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-blue">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <img src="header-logo.png" alt="로고 이미지" height="40" />
+                </Link>
 
-                    <button className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarTogglerDemo02"
-                        aria-controls="navbarTogglerDemo02"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <button className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                    {user && (
-                        <div className="collapse navbar-collapse justify-center" id="navbarTogglerDemo02">
-                            <form className="d-flex" action="/search" method="GET">
-                                <input className="form-control me-2 width-[30] height-[50px] rounded-[30px]" type="search" placeholder="검색" aria-label="Search" name="q"></input>
-                            </form>
-                        </div>
-                    )}
-
-                    <div className="right-text">
-                        {user ? (
-                            <button className="pr-[40px]" onClick={handleLogout}>로그아웃</button>
-                        ) : (
-                            <Link className="pr-[40px]" to="/login">로그인</Link>
-                        )}
+                {user && (
+                    <div className="collapse navbar-collapse justify-center" id="navbarTogglerDemo02">
+                        <form className="d-flex" action="/search" method="GET">
+                            <input className="form-control me-2 width-[30] height-[50px] rounded-[30px]" type="search" placeholder="검색" aria-label="Search" name="q"></input>
+                        </form>
                     </div>
+                )}
 
+                <div className="right-text">
+                    {user ? (
+                        <button className="pr-[40px]" onClick={handleLogout}>로그아웃</button>
+                    ) : (
+                        <Link className="pr-[40px]" to="/login">로그인</Link>
+                    )}
                 </div>
-            </nav>
-        </header>
+
+            </div>
+        </nav>
     );
 };
 
